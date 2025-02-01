b82f451eca890978c847bc4d950632944e1689ca
DezmenSykes <oddly.observant@gmail.com>
2025-02-01 10:55:41 -0600

    Updated index.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
       }>
       
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome to your new app!</ThemedText>
+        <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
       </ThemedView>
     </ParallaxScrollView>
