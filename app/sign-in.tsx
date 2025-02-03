b8e3fbbcc05f207a93cfdc4404697e98a19105e7
DezmenSykes <oddly.observant@gmail.com>
2025-02-03 17:17:29 -0600

    for updates to sign in

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -5,9 +5,16 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 
 const SignIn = () => {
   return (
-    <SafeAreaView className='bg-white h-full'>
-      <ScrollView contentContainerClassName='h-full'>
-        <Image source={images.onboarding} className='w-full h-4/6' resizeMode='contain' />
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image source={images.onboarding} className="w-full h-3/4" resizeMode="contain"/>
+        <View className='px-10'>
+          <Text className='text-base text-center uppercase font-rubik text-black-200'>Welcome to Utopify</Text>
+          <Text className='text-3xl font-rubik-bold text-black-300 text-center mt-2'>
+            Let's Get You Closer to {'\n'}
+            <Text>Your Ideal Home</Text>
+          </Text>
+        </View>
       </ScrollView>
     </SafeAreaView>
   )
