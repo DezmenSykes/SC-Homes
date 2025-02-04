5e04739cba9515ed34d7a5f8770806ec564a00dd
DezmenSykes <oddly.observant@gmail.com>
2025-02-04 10:54:26 -0600

    taiwind color update

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -7,12 +7,12 @@ const SignIn = () => {
   return (
     <SafeAreaView className="bg-white h-full">
       <ScrollView contentContainerClassName="h-full">
-        <Image source={images.onboarding} className=" " resizeMode="contain"/>
+        <Image source={images.onboarding} className="w-full h-4/6" resizeMode="center"/>
         <View className='px-10'>
           <Text className='text-base text-center uppercase font-rubik text-black-200'>Welcome to Utopify</Text>
           <Text className='text-3xl font-rubik-bold text-black-300 text-center mt-2'>
             Let's Get You Closer to {'\n'}
-            <Text>Your Ideal Home</Text>
+            <Text className='text-primary-300'>Your Ideal Home</Text>
           </Text>
         </View>
       </ScrollView>
